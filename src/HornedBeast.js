83e6f08710aaa1b4c1dcb6987fc82303e9b2bdb6
Dennis DeVries <7dennisdv7@gmail.com>
2022-06-16 22:12:13 -0400

    adding some styling to hornedbeast

--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
+import Col from 'react-bootstrap/Col';
 import './HornedBeast.css';
 
 class HornedBeast extends React.Component {
@@ -21,21 +22,23 @@ class HornedBeast extends React.Component {
 
   render () {
     return (
-      <Card style={{ width: '18rem' }}>
-      <Card.Img 
-        variant="top" 
-        src={this.props.image_url} 
-        onClick={() => {this.props.openModal(this.props.beast)}} 
-        alt={this.props.alt} 
-        title={this.props.title}
-      />
-      <Card.Text>❤️ {this.state.votes}</Card.Text>
-      <Card.Body>
-        <Card.Title>{this.props.title}</Card.Title>
-        <Card.Text>{this.props.description}</Card.Text>
-        <Button onClick={this.handleVotes} variant="primary">Button</Button>
-      </Card.Body>
-    </Card>
+      <Col classname="mt-4">
+        <Card className="h-100 p-3" style={{ width: '18rem' }}>
+        <Card.Img 
+          variant="top" 
+          src={this.props.image_url} 
+          onClick={() => {this.props.openModal(this.props.beast)}} 
+          alt={this.props.alt} 
+          title={this.props.title}
+        />
+        <Card.Text>❤️ {this.state.votes}</Card.Text>
+        <Card.Body>
+          <Card.Title>{this.props.title}</Card.Title>
+          <Card.Text>{this.props.description}</Card.Text>
+          <Button className='mt-3' onClick={this.handleVotes} variant="primary">Button</Button>
+        </Card.Body>
+      </Card>
+    </Col>
     )
   }
 }
